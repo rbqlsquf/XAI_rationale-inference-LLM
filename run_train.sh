32b2358b4943a5d8e1bb59c8c0d28b793f26742a
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-10-27 17:24:25 +0900

    train sh

--- run_train.sh
+++ run_train.sh
@@ -8,8 +8,9 @@ SOURCE_DIR="$BASE_DIR/source"
 
 $PYTHON_PATH $SOURCE_DIR/train_pn.py \
     --data_file $BASE_DIR/data/hotpot_cnn_6k.json \
-    --new_model qwen_lora_1025 \
-    --output_dir /hdd/rbqlsquf/qwen_lora_1025 \
+    --new_model qwen_lora_1026 \
+    --output_dir qwen_lora_1026 \
     --num_train_epochs 1 \
     --batch_size 4 \
-    --gradient_accumulation_steps 1
+    --gradient_accumulation_steps 1 \
+    --data_sample False
