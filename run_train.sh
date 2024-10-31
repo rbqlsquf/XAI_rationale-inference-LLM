254e2342d022ea81de6fe84b6c9aef0ed0b52760
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-10-31 15:30:16 +0900

    +loss

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
-PYTHON_PATH="/home/rbqlsquf/workspace/XAI_rationale-inference-LLM/.venv/bin/python"
+PYTHON_PATH="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM/.venv/bin/python"
 
-BASE_DIR="/home/rbqlsquf/workspace/XAI_rationale-inference-LLM"
+BASE_DIR="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM"
 MODEL_DIR="$BASE_DIR/model"
 OUTPUT_DIR="$BASE_DIR/result"
 SOURCE_DIR="$BASE_DIR/source"
@@ -9,8 +9,8 @@ SOURCE_DIR="$BASE_DIR/source"
 $PYTHON_PATH $SOURCE_DIR/train_pn.py \
     --data_file "$BASE_DIR/data/1022data/hotpot_cnn_6k.json" \
     --new_model "qwen_lora_1031" \
-    --output_dir "/hdd/rbqlsquf/qwen_lora_1031" \
+    --output_dir "qwen_lora_1031_" \
     --num_train_epochs 1 \
     --batch_size 4 \
     --gradient_accumulation_steps 1 \
-    --wandb_run_name "1031" \
+    --wandb_run_name "1031+loss" \
