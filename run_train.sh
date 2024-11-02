ba6b001b3a4c1960a9a4b0a976225ec08d38f749
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-11-02 19:10:08 +0900

    train.noloss

--- run_train.sh
+++ run_train.sh
@@ -6,13 +6,11 @@ OUTPUT_DIR="$BASE_DIR/result"
 SOURCE_DIR="$BASE_DIR/source"
 
 
-# $PYTHON_PATH $SOURCE_DIR/train_pn.py \
-#     --data_file "$BASE_DIR/data/1022data/hotpot_cnn_6k.json" \
-#     --new_model "qwen_lora_1101" \
-#     --output_dir "/hdd/rbqlsquf/qwen_lora_1102+loss" \
-#     --num_train_epochs 1 \
-#     --batch_size 2 \
-#     --gradient_accumulation_steps 1 \
-#     --wandb_run_name "1102" \
-
-$PYTHON_PATH $SOURCE_DIR/train_origin.py \
+$PYTHON_PATH $SOURCE_DIR/train_pn_noloss.py \
+    --data_file "$BASE_DIR/data/train_hotpot_cnn_1022.json" \
+    --new_model "1102+dataup" \
+    --output_dir "/hdd/rbqlsquf/1102+dataup" \
+    --num_train_epochs 1 \
+    --batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --wandb_run_name "1102+dataup" \
