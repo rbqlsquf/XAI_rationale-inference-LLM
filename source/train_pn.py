d126a1b9e4d04d01b8f67ee8a3e0396cc25e4956
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-11-04 16:06:02 +0900

    code

--- source/train_pn.py
+++ source/train_pn.py
@@ -311,13 +311,13 @@ class CustomTrainer(Trainer):
         if (
             torch.mean(evidence_nll).item() != 0
             and torch.mean(evidence_nll).item() < 1000
-            and torch.isnan(torch.mean(evidence_nll).item())
+            and not torch.any(torch.isnan(evidence_nll))
         ):
             loss = loss + 0.1 * evidence_nll
         if (
             torch.mean(g_evidence_nll).item() != 0
-            and torch.mean(evidence_nll).item() < 1000
-            and torch.isnan(torch.mean(evidence_nll).item())
+            and torch.mean(g_evidence_nll).item() < 1000
+            and not torch.any(torch.isnan(g_evidence_nll))
         ):
             loss = loss + 0.1 * g_evidence_nll
 
@@ -482,7 +482,7 @@ if __name__ == "__main__":
     ##############################################################
     parser = argparse.ArgumentParser(description="인자값을 전달받는 Python 스크립트")
     parser.add_argument("--model_path", type=str, default="Qwen/Qwen2.5-3B-Instruct")
-    parser.add_argument("--data_file", type=str, default="data/train_hotpot_cnn_1022.json")
+    parser.add_argument("--data_file", type=str, default="data/1020data/train_hotpot_cnn_filtered.json")
     parser.add_argument("--beam_size", type=int, default=1)
     parser.add_argument("--max_dec_len", type=int, default=3)
     parser.add_argument("--new_model", type=str, default="new_model")
@@ -492,7 +492,7 @@ if __name__ == "__main__":
     parser.add_argument("--num_train_epochs", type=int, default=1)
     parser.add_argument("--batch_size", type=int, default=2)
     parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
-    parser.add_argument("--data_sample", type=bool, default=False)
+    parser.add_argument("--data_sample", type=bool, default=True)
     args = parser.parse_args()
     print(args)
     #########################################################
