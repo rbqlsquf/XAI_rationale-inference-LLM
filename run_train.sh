3e344eb42a98304e577f52bbde37eeb90ae7a6db
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-11-07 10:30:49 +0900

    element_sum

--- run_train.sh
+++ run_train.sh
@@ -1,16 +1,16 @@
-PYTHON_PATH="/home/rbqlsquf/workspace/XAI_rationale-inference-LLM/.venv/bin/python"
+PYTHON_PATH="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM/.venv/bin/python"
 
-BASE_DIR="/home/rbqlsquf/workspace/XAI_rationale-inference-LLM"
+BASE_DIR="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM"
 MODEL_DIR="$BASE_DIR/model"
 OUTPUT_DIR="$BASE_DIR/result"
 SOURCE_DIR="$BASE_DIR/source"
 
 
-$PYTHON_PATH $SOURCE_DIR/train_pn.py \
+$PYTHON_PATH $SOURCE_DIR/train_pn_noloss.py \
     --data_file $BASE_DIR/data/1020data/train_hotpot_cnn_filtered.json \
-    --new_model /hdd/rbqlsquf/loss+dataup+att \
-    --output_dir /hdd/rbqlsquf/loss+dataup+att \
+    --new_model 1106_weighted_rationale+yesloss_ \
+    --output_dir 1106_weighted_rationale+yesloss \
     --num_train_epochs 1 \
     --batch_size 2 \
     --gradient_accumulation_steps 1 \
-    --wandb_run_name loss+dataup+att
+    --wandb_run_name 1106_weighted_rationale+yesloss
