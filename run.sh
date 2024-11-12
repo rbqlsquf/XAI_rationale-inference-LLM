a18c1d82337d747f69aee8c710733ab79c77d7ce
rbqlsquf <rbqlsquf@konkuk.ac.kr>
2024-11-12 11:02:08 +0900

    yes_loss eval

--- run.sh
+++ run.sh
@@ -1,27 +1,27 @@
 PYTHON_PATH="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM/.venv/bin/python"
 
 BASE_DIR="/home/rbqlsquf2/workspace/XAI_rationale-inference-LLM"
-MODEL_DIR="1106_weighted_sum"
-OUTPUT_DIR="$BASE_DIR/result/1106_weighted_sum"
+MODEL_DIR="model/1111_1107_yesloss_concat"
+OUTPUT_DIR="$BASE_DIR/result/1111_1107_yesloss_concat"
 SOURCE_DIR="$BASE_DIR/source"
-SCRIPT_NAME="inference_pn_att_1106_sum.py"
+SCRIPT_NAME="inference_1108.py"
 
-# for i in {1..10}; do
-#     checkpoint=$((i * 1000))
-#     $PYTHON_PATH $SOURCE_DIR/$SCRIPT_NAME \
-#         --mrc_value "False" \
-#         --sum_value "True" \
-#         --train_model_path "$MODEL_DIR/checkpoint-$checkpoint" \
-#         --output_dir "$OUTPUT_DIR/hotpot_ft_$checkpoint.json"
-# done
-
-
-for i in 1 3 7; do
-    checkpoint=$((i * 1000))
+for i in {30..60..10}; do
+    checkpoint=$((i * 100))
     $PYTHON_PATH $SOURCE_DIR/$SCRIPT_NAME \
-        --data_file "data/1029data/cnn_dev.json" \
         --mrc_value "False" \
         --sum_value "True" \
         --train_model_path "$MODEL_DIR/checkpoint-$checkpoint" \
         --output_dir "$OUTPUT_DIR/hotpot_ft_$checkpoint.json"
 done
+
+
+# for i in 1 3 7; do
+#     checkpoint=$((i * 100))
+#     $PYTHON_PATH $SOURCE_DIR/$SCRIPT_NAME \
+#         --data_file "data/1029data/cnn_dev.json" \
+#         --mrc_value "False" \
+#         --sum_value "True" \
+#         --train_model_path "$MODEL_DIR/checkpoint-$checkpoint" \
+#         --output_dir "$OUTPUT_DIR/hotpot_ft_$checkpoint.json"
+# done
